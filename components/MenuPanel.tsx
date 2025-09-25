979a12873c347b9d28c72044a427b33426fb3e2d
Shykat Raha <devshykat2022@gmail.com>
2025-09-25 14:01:34 +0600

    Fix: position of theme toggler

--- components/MenuPanel.tsx
+++ components/MenuPanel.tsx
@@ -4,19 +4,21 @@ import { cn } from "@/lib/utils";
 import { MoonIcon, SunIcon } from "lucide-react";
 
 const MenuPanel = () => {
-  const { menuOpen, theme, setTheme } = useMenu();
+  const { menuOpen, setMenuOpen, theme, setTheme } = useMenu();
   return (
     <div
       className={cn(
-        "text-amber-50 fixed bottom-0 right-4 rounded-t-2xl sm:right-8 md:right-16 z-50 bg-black/30 border-2 border-white/5 border-b-0 w-fit h-fit flex flex-col gap-y-1 transition-all duration-300 ease-in-out backdrop-blur-2xl divide-y divide-white/10",
-        menuOpen ? "translate-y-0" : "translate-y-full"
+        "text-amber-50 fixed bottom-0 md:top-0 right-4 rounded-t-2xl md:rounded-t-none md:rounded-b-2xl sm:right-8 md:right-14 z-40 bg-black/50 md:bg-black/30 border-2 border-white/5 border-b-0 w-fit h-fit flex flex-col gap-y-1 md:pt-18 transition-all duration-300 ease-in-out backdrop-blur-2xl divide-y divide-white/10",
+        menuOpen ? "translate-y-0" : "translate-y-full md:-translate-y-full"
       )}
     >
-      <span className="px-4 py-2">Accessiblity Menu</span>
-
-      <div className="p-1 w-full">
+      <div className="p-1.5 w-full">
         <button
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          aria-label="Theme toggler"
+          onClick={() => {
+            setTheme(theme === "light" ? "dark" : "light");
+            setMenuOpen(false);
+          }}
           className="bg-black/30 w-full py-2 px-4 rounded-xl cursor-pointer flex items-center gap-4"
         >
           {theme === "light" ? <MoonIcon size={20} /> : <SunIcon size={20} />}
